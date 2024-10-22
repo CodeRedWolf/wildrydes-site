269e4acc7ad6d445f3db8f1ad6d6c29f272b4427
CodeRedWolf <joncolemanjc888@gmail.com>
2024-10-21 19:31:50 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_8QKu56E2m', // e.g. us-east-1_8QKu56E2m
+        userPoolClientId: '5khj0m98giv3dgeksp00vcpc3k', // e.g. 5khj0m98giv3dgeksp00vcpc3k
+
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
